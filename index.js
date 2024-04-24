6a9a7b62d4f827302cd5aa4075ed3b22c7dd6d90
Zohaib <www.zohaib.0335@gmail.com>
2024-04-24 23:10:53 +0500

    add

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server listening on port: ", PORT);
+});
+
+app.get("/status", (request, response) => {
+  const status = {
+    status: "running",
+  };
+  response.send(status);
+});
